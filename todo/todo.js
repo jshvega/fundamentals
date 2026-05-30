e8cac95518bbe29baa40e460c7dc441797ad31bb
Manfred <jsh.vega11@gmail.com>
2026-05-29 21:58:38 -0600

    Refactor: move taks to array-based state model with index-linked DOM

--- todo/todo.js
+++ todo/todo.js
@@ -3,6 +3,9 @@ const listContainer = document.getElementById("list-container")
 const empty = document.getElementById("empty")
 
 
+let tasksArr = []
+
+
 function isEmpty(){
     if(listContainer.querySelectorAll('[data-li]').length === 0){
         empty.style.display = 'block'
@@ -13,20 +16,39 @@ function isEmpty(){
     }
 }
 
+function render(){
+
+    listContainer.innerHTML = ''
+
+    tasksArr.forEach((task, index) => {
+        if(task.checked){
+            listContainer.innerHTML += `
+            <li class="li-checked" data-li="li" data-index="${index}">
+                <div class="li-radio li-radio-checked" data-radio="radio"></div>
+                <p data-item="item">${tasksArr[index].task}</p>
+                <button class="li-remove" data-remove="remove"></button>
+            </li> `
+        } else{
+            listContainer.innerHTML += `
+            <li class="" data-li="li" data-index="${index}">
+                <div class="li-radio" data-radio="radio"></div>
+                <p data-item="item">${tasksArr[index].task}</p>
+                <button class="li-remove" data-remove="remove"></button>
+            </li> `
+        }
+    })
+
+}
+
 function addTask(){
     if(inputField.value === ''){
         alert("Please write a task.")
     } else{
-        let newItem = `
-        <li class="" data-li="li">
-            <div class="li-radio" data-radio="radio"></div>
-            <p data-item="item">${inputField.value}</p>
-            <button class="li-remove" data-remove="remove"></button>
-        </li> `
-        listContainer.innerHTML += newItem
-        inputField.value = ''
+        tasksArr.push({checked: false, task: inputField.value})
     }
+    render()
     isEmpty()
+    inputField.value = ''
 }
 
 
@@ -40,13 +62,16 @@ listContainer.addEventListener("click", function(e){
     e.preventDefault()
     const parentLi = e.target.closest('[data-li]')
 
+    // Remove
     if(e.target.dataset.remove){
-        parentLi.remove()
+        tasksArr.splice(parentLi.dataset.index, 1)
+        render()
         isEmpty()
     }
 
+    // Check/Toggle
     if(e.target.dataset.item || e.target.dataset.radio){
-        parentLi.querySelector(".li-radio").classList.toggle('li-radio-checked')
-        parentLi.classList.toggle('li-checked')
+        tasksArr[parentLi.dataset.index].checked = !tasksArr[parentLi.dataset.index].checked
+        render()
     }
 })
